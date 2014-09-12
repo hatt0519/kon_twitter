bc4ed1946ddb4d0f8a4caf5a3602f3000653833b
服部 一樹 <moroku0519@gmail.com>
2014-09-12 17:37:48 +0900

    予約システムへのショートカット追加

--- twitter.php
+++ twitter.php
@@ -18,7 +18,9 @@ $access_token_secret = "JZ5gghLrHzBh6cdx3bDCx5hGvukGY7lKRfkcnkVNJV30S";
 // OAuthオブジェクト生成
 $to = new TwitterOAuth($consumer_key,$consumer_secret,$access_token,$access_token_secret);
 
-$time = date( "H時i分", time() );
+$today = new DateTime();
+
+$time = $today->format("H時i分");
 
 $announce = $time."現在の部室の空き状況をお知らせします。";
 
@@ -27,7 +29,7 @@ $req = $to->OAuthRequest("https://api.twitter.com/1.1/statuses/update.json","POS
 
 
 foreach ($Available_Room as $key => $val) {
-	$message = $time."現在、".$val->period."限の".$val->room."室が空いています\n ";
+	$message = $time."現在、".$val->period."限の".$val->room."室が空いています\n予約はこちらからhttp://www.kendai-kon.info/new_input.cgi?id=".$val->id."&week_id=".$val->week_id;
 	//投稿
 	$req = $to->OAuthRequest("https://api.twitter.com/1.1/statuses/update.json","POST",array("status"=>$message));
 }
