e85fb9c0c5c254201724ba3522105c0a4f6880e8
服部 一樹 <moroku0519@gmail.com>
2014-09-26 22:57:56 +0900

    時間帯によって発生する満室に対応

--- twitter.php
+++ twitter.php
@@ -5,7 +5,6 @@ require_once("Curl.class.php");
 
 $Curl = new Curl();
 
-$Available_Room = json_decode($Curl->GetAvailableRoom());
 $holiday_checker = json_decode($Curl->GetholidayChecker());
 $ban_checker = json_decode($Curl->GetbanChecker());
 $next_day_schedule = json_decode($Curl->GetNextDay());
@@ -23,57 +22,54 @@ $to = new TwitterOAuth($consumer_key,$consumer_secret,$access_token,$access_toke
 
 $today = new DateTime();
 
-$time = $today->format("H時i分");
-//$time = "08時45分";
+//$time = $today->format("H時i分");
+$time = "14時30分";
+
 if($ban_checker->ban_flg == 1){
 	$ban = "ごめんね、残念だけど今日は部室の使用禁止なんだ。。。";
 	$req = $to->OAuthRequest("https://api.twitter.com/1.1/statuses/update.json","POST",array("status"=>$ban));
 }else{	
 
-	if($time == "08時00分"){
-
-		if(empty($holiday_checker)){
-			$greeting = "部員のみなさん!!今日休日日程だから気をつけてね!!1限と3限以外開始時刻が違うよ!!";
-		}else{
-			$greeting = "部員のみなさんおはよう!!今日も一日がんばろうね!!";
-		}
-		$req = $to->OAuthRequest("https://api.twitter.com/1.1/statuses/update.json","POST",array("status"=>$greeting));
-	}
-
-	if (!empty($Available_Room)){
-		$announce = "部員のみなさん！！".$time."現在の部室の空き状況を教えるね！！";
-	}else{
-		$announce = $time."お姉ちゃん！！残念だけど今空いている部室はないよ。。。";
-	}
-
+	$announce = "部員のみなさん！！".$time."現在の部室の空き状況を教えるね！！";
 	//投稿
 	$req = $to->OAuthRequest("https://api.twitter.com/1.1/statuses/update.json","POST",array("status"=>$announce));
 
-	foreach ($Available_Room as $key => $val) {
-		if ($time == "12時10分"){
-			if($val->period > 2){
-				$message = $time."現在、".$val->period."限の".$val->room."室が空いてるよ！！\n予約はこちらからしてね！！http://www.kendai-kon.info/new_input.cgi?id=".$val->id."&week_id=".$val->week_id;
+	switch($time){
+		case "08時00分":
+			$Available_Room = json_decode($Curl->GetAvailableRoom());
+			if(empty($holiday_checker)){
+				$greeting = "部員のみなさん!!今日休日日程だから気をつけてね!!1限と3限以外開始時刻が違うよ!!";
+			}else{
+				$greeting = "部員のみなさんおはよう!!今日も一日がんばろうね!!";
 			}
-		}elseif($time == "14時30分"){
-			if($val->period > 3){
-				$message = $time."現在、".$val->period."限の".$val->room."室が空いてるよ！！\n予約はこちらからしてね！！http://www.kendai-kon.info/new_input.cgi?id=".$val->id."&week_id=".$val->week_id;
-			}
-		}elseif($time == "16時10分"){
-			if($val->period > 4){
-				$message = $time."現在、".$val->period."限の".$val->room."室が空いてるよ！！\n予約はこちらからしてね！！http://www.kendai-kon.info/new_input.cgi?id=".$val->id."&week_id=".$val->week_id;
-			}
-		}elseif ($time == "17時50分") {
-			if($val->period > 5){
-				$message = $time."現在、".$val->period."限の".$val->room."室が空いてるよ！！\n予約はこちらからしてね！！http://www.kendai-kon.info/new_input.cgi?id=".$val->id."&week_id=".$val->week_id;
-			}
-		}else{
+			$req = $to->OAuthRequest("https://api.twitter.com/1.1/statuses/update.json","POST",array("status"=>$greeting));
+			break;
+		case "12時10分":
+			$Available_Room = json_decode($Curl->GetAvailableRoom_12_10());
+			break;
+		case "14時30分":
+			$Available_Room = json_decode($Curl->GetAvailableRoom_14_30());
+			break;
+		case "16時10分":
+			$Available_Room = json_decode($Curl->GetAvailableRoom_16_10());
+			break;
+		case "17時50分":
+			$Available_Room = json_decode($Curl->GetAvailableRoom_17_50());
+			break;
+	}
+	if(!empty($Available_Room)){
+		foreach($Available_Room as $key => $val){
 			$message = $time."現在、".$val->period."限の".$val->room."室が空いてるよ！！\n予約はこちらからしてね！！http://www.kendai-kon.info/new_input.cgi?id=".$val->id."&week_id=".$val->week_id;
+			$req = $to->OAuthRequest("https://api.twitter.com/1.1/statuses/update.json","POST",array("status"=>$message));
 		}
+	}else{
+		$message = "ごめんね、今空いてる部室ないんだ。。。";
 		$req = $to->OAuthRequest("https://api.twitter.com/1.1/statuses/update.json","POST",array("status"=>$message));
 	}
 
+}
+	
 
 	// レスポンスを表示する場合は下記コメントアウトを外す
 	//header("Content-Type: application/xml");
 	//echo $req;
-}
