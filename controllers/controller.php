555c1671fd6a3729e3e6d8b4d4c003d2d1b4b497
服部一樹 <kazuki@hattorikazuki-no-MacBook-Air.local>
2016-06-06 23:11:21 +0900

    不要ファイル、ディレクトリの削除

--- controllers/controller.php
+++ controllers/controller.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Controller;
+namespace Controllers;
 
 use \Kon\Checker;
 use \Kon\MessageMaker;
@@ -8,15 +8,14 @@ use \Kon\Tweet;
 
 class Controller
 {
-    public function run($time, $today, $selected_sister, $sister, $period, $json)
+    const WEATHER_URI = 'http://www.drk7.jp/weather/xml/22.xml';
+    const WARNING_URI = 'http://weather.livedoor.com/forecast/rss/warn/22.xml';
+    const TRAIN_URI = 'https://rti-giken.jp/fhc/api/train_tetsudo/delay.json';
+    public static function run($time, $today, $selected_sister, $sister, $json)
     {
-        $month = $today->format('m');
-        $day = $today->format('d');
-        $weather_uri = 'http://www.drk7.jp/weather/xml/22.xml';
-        $train_uri = 'https://rti-giken.jp/fhc/api/train_tetsudo/delay.json';
-        $ban_checker_uri = 'localhost:4567/checker.json?month='.$month.'&day='.$day.'&search_type=ban';
-        $holiday_checker_uri = 'localhost:4567/checker.json?month='.$month.'&day='.$day.'&search_type=holiday';
-        $available_room_uri = 'localhost:4567/available_room.json?month='.$month.'&day='.$day;
+        $ban_checker_uri = 'localhost:4567/checker.json?month='.$today->format('m').'&day='.$today->format('d').'&search_type=ban';
+        $holiday_checker_uri = 'localhost:4567/checker.json?month='.$today->format('m').'&day='.$today->format('d').'&search_type=holiday';
+        $available_room_uri = 'localhost:4567/available_room.json?month='.$today->format('m').'&day='.$today->format('d');
         $is_ban = Checker::is_ban($ban_checker_uri);
         $is_holiday = Checker::is_holiday($holiday_checker_uri);
         $message = new MessageMaker($selected_sister, $is_holiday, $is_ban);
@@ -32,37 +31,57 @@ class Controller
                 $tweet->tweetMessage($shift_message);
                 $tweet->changeProfile($selected_sister);
                 break;
-            case "08時30分": case "22時30分":
-                $weather_message = $message->weatherNews($time, $month, $day);
+            case "08時30分":
+                $weather_message = $message->weatherNews($time, $today->format('m'), $today->format('d'), self::WEATHER_URI, self::WARNING_URI);
+                $tweet->tweetMessage($weather_message);
+                break;
+            case "22時30分":
+                $today->modify('+1days');
+                $weather_message = $message->weatherNews($time, $today->format('m'), $today->format('d'), self::WEATHER_URI, self::WARNING_URI);
                 $tweet->tweetMessage($weather_message);
                 break;
             default:
                 switch($time)
                 {
                     case "08時00分":
-                        $default_message_plus = $message->default_morning($holiday_checker->check_holiday());
+                        $default_message_plus = $message->default_morning();
+                        $default_message = $message->default_other($time);
                         break;
                     case "12時10分":
-                        $default_message_plus = $message->default_noon($holiday_checker->check_holiday());
+                        $default_message_plus = $message->default_noon();
+                        $available_room_uri .= '&period=3&search_range=after';
+                        $default_message = $message->default_other($time);
+                        break;
+                    case "14時30分":
+                        $available_room_uri .= '&period=4&search_range=after';
+                        $default_message = $message->default_other($time);
+                        break;
+                    case "16時10分":
+                        $available_room_uri .= '&period=5&search_range=after';
+                        $default_message = $message->default_other($time);
+                        break;
+                    case "17時50分":
+                        $default_message = $message->default_other($time);
+                        $available_room_uri .= '&period=6&search_range=after';
                         break;
                     case "22時00分":
                         $today->modify('+1days');
-                        $default_message_plus = $message->default_midnight($holiday_checker->check_holiday());
+                        $default_message_plus = $message->default_midnight();
+                        $available_room_uri = 'localhost:4567/available_room.json?month='.$today->format('m').'&day='.$today->format('d');
                         break;
                     default:
                         break;
                 }
-                $default_message = $message->default_other($time);
-                $available_room_message = $message->available_room($time);
-                array_unshift($available_room_message, $default_message);
-                if(isset($default_message_plus)) { array_unshift($available_room_message, $default_message_plus); };
+                $available_room_message = $message->available_room($time, $available_room_uri);
+                if(isset($default_message)) array_unshift($available_room_message, $default_message);
+                if(isset($default_message_plus)) array_unshift($available_room_message, $default_message_plus);
                 $tweet->tweetMessageArray($available_room_message);
                 break;
         }
         if($time != "07時00分")
         {
-            $message->trainInfo($train_uri);
-            $tweet->tweetMessage($message);
+            $train_info = $message->trainInfo(self::TRAIN_URI);
+            if(!empty($train_info)) $tweet->tweetMessage($message);
         }
     }
 }
